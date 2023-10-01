217c24d62d72ef33855337e35b1a2b6e1cb5c8d1
kamal <kamal.swami.22062001@gmail.com>
2023-10-01 22:17:40 +0530

    Locators

--- Basic Interactions/Basic Selenium Task.py
+++ Basic Interactions/Basic Selenium Task.py
@@ -1,10 +1,10 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
-import time
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.action_chains import ActionChains
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as ec
 
 # Open Google, maximise window and Search Jai HO !! {Chrome Browser}
 
