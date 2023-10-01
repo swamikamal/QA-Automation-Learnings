2178e4b3c6f0ec795ead18a1f4e656532d9b3ff3
kamal <kamal.swami.22062001@gmail.com>
2023-10-01 12:29:18 +0530

    Basic Task 1 driver

--- Basic Interactions/Basic Selenium Task.py
+++ Basic Interactions/Basic Selenium Task.py
@@ -3,17 +3,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
 import time
 # Open Google, maximise window and Search Jai HO !! {Chrome Browser}
 driver = webdriver.Chrome()
 
-OpenLink="https://www.google.com/"
+OpenLink = "https://www.google.com/"
 
 driver.get(OpenLink)
 time.sleep(2)
 driver.maximize_window()
-elem=driver.find_element(By.XPATH,'//*[@id="APjFqb"]')
+elem = driver.find_element(By.XPATH, '//*[@id="APjFqb"]')
 elem.send_keys("Jai Ho!!")
 elem.send_keys(Keys.ENTER)
 time.sleep(5)
